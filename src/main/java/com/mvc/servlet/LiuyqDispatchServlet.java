8cd28011763abc0c67f6b5293e5a4eee734b9418
liuyq913 <2273852279@qq.com>
2018-08-29 21:24:06 +0800

    spring

---
+++ src/main/java/com/mvc/servlet/LiuyqDispatchServlet.java
@@ -0,0 +1,24 @@
+package com.mvc.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LiuyqDispatchServlet extends HttpServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+}
